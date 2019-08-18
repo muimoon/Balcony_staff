a579497c0deea3980616518230f83c82ee6f11c0
谬老汉 <muimoon@gmail.com>
2019-08-18 18:31:57 +0800

    updated

--- Balcony_staff.ino
+++ Balcony_staff.ino
@@ -4,17 +4,10 @@ int Spraying_Pin = 4;
 int Light_Pin = 6;
 int sensorValue = 0;
 int RXLED = 17;
-// Turn on (true) or off (false) the Tx and Rx led.
-// ------------------------------------------------
-// They are turned off by making them input.
-// That prevents that any RX or TX activity will turn them on.
-// However, when the leds are on, any RX or TX action will
-// make them blink or turn them off.
 void ledTx(boolean on)
 {
   if (on)
   {
-    // led on. The led is connected to VCC. Make pin low to turn led on.
     pinMode(LED_BUILTIN_TX, OUTPUT); // pin as output.
     digitalWrite(LED_BUILTIN_TX, LOW); // pin low
     // These two lines will do the same:
@@ -23,10 +16,6 @@ void ledTx(boolean on)
   }
   else
   {
-    // led off
-    // turn it off, by setting it as input, so the serial activity can't turn it on.
-    // If the internal pullup resistor is enabled or not, that does not matter,
-    // since the led it connected to VCC.
     pinMode(LED_BUILTIN_TX, INPUT);
     // This line will do the same:
     // bitClear( DDRD, 5);        // set pin as input
@@ -71,8 +60,11 @@ void loop()
       digitalWrite(Light_Pin, HIGH);
     }
     digitalWrite(Spraying_Pin, HIGH);
+    delay(60000);
   }
-  delay(30000);
-  digitalWrite(Light_Pin, LOW);
-  digitalWrite(Spraying_Pin, LOW);
+  else
+  {
+    digitalWrite(Light_Pin, LOW);
+    digitalWrite(Spraying_Pin, LOW);
+  }
 }
