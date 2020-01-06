de8aeb7382d07f0b6b7c94791888fc4607bc0cb2
muimoon <muimoon@gmail.com>
2020-01-06 17:00:06 +0800

    Create code.ino

---
+++ code/code.ino
@@ -0,0 +1,52 @@
+int Bright_sensor = A0;
+int Pir_sensor = 2;
+int Spraying_Pin = 4;
+int Light_Pin = 6;
+int sensorValue = 0;
+int PersonValue;
+unsigned long previousMillis = 0;
+// Intervalo de medicao temperatura
+const long interval = 6000;
+void PersonCome(boolean come)
+{
+  if (come)
+  {
+    if (sensorValue > 800)
+    {
+      digitalWrite(Light_Pin, HIGH);
+    }
+    digitalWrite(Spraying_Pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(Light_Pin, LOW);
+    digitalWrite(Spraying_Pin, LOW);
+  }
+}
+
+void setup()
+{
+  PersonCome(false);
+  // initialize both leds to be off
+  pinMode(Pir_sensor, INPUT);
+  pinMode(Spraying_Pin, OUTPUT);
+  pinMode(Light_Pin, OUTPUT);
+  digitalWrite(Spraying_Pin, LOW);
+  digitalWrite(Light_Pin, LOW);
+}
+
+void loop()
+{
+  PersonValue = digitalRead(Pir_sensor);
+  sensorValue = analogRead(Bright_sensor);
+  if (PersonValue == 1)
+  {
+    PersonCome(true);
+  }
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    PersonCome(false);
+    previousMillis = currentMillis;
+  }
+}
